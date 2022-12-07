fb252dd7bee9309380c270d5fc4c9af2cd8ec2f6
hsjaehun <hsjaehun@gmail.com>
2022-12-08 08:13:51 +0900

    commit

--- main.js
+++ main.js
@@ -4,25 +4,40 @@ const app = express();
 const { SerialPort } = require("serialport");
 
 // ls /dev/tty.*  : to check all the connected ports.
+// 1 Channel USB Relay - 10A
+// http://vctec.co.kr/product/detail.html?product_no=14806&cate_no=117&display_group=1&cafe_mkt=naver_ks&mkt_in=Y&ghost_mall_id=naver&ref=naver_open&NaPm=ct%3Dlavx3qwo%7Cci%3D6a8cb8b60f29d4e1db94cacfd2b7277b5475e0f8%7Ctr%3Dsls%7Csn%3D246260%7Chk%3D2b3948a561c04ab628611a5959bbd5f1613a8698
 
-function createLocalServer() {
-   // 1 Channel USB Relay - 10A
-   // http://vctec.co.kr/product/detail.html?product_no=14806&cate_no=117&display_group=1&cafe_mkt=naver_ks&mkt_in=Y&ghost_mall_id=naver&ref=naver_open&NaPm=ct%3Dlavx3qwo%7Cci%3D6a8cb8b60f29d4e1db94cacfd2b7277b5475e0f8%7Ctr%3Dsls%7Csn%3D246260%7Chk%3D2b3948a561c04ab628611a5959bbd5f1613a8698
-   const port = new SerialPort({
-      path: "/dev/tty.usbserial-14420",
-      baudRate: 9600,
-   });
-   app.get("/", async (req, res) => {
-      // turn on
+async function createLocalServer() {
+   let port;
+   try {
+      const serialList = await SerialPort.list();
+      const portPath = serialList.find((port) =>
+         port.path.includes("/dev/tty.usbserial-")
+      ).path;
+      port = new SerialPort({
+         path: portPath,
+         baudRate: 9600,
+      });
+   } catch (e) {
+      console.log(e);
+   }
+
+   app.get("/control/:action", (req, res) => {
+      const action = req.params.action;
       const turnOn = [0xa0, 0x01, 0x01, 0xa2];
       const turnOff = [0xa0, 0x01, 0x00, 0xa1];
-      port.write(turnOn);
-      // turn off after 2 secs
-      await new Promise(() => {
-         setTimeout(() => {
+      switch (action) {
+         case "turnon":
+            port.write(turnOn);
+            res.send("turned on");
+            break;
+         case "turnoff":
             port.write(turnOff);
-         }, 2000);
-      });
+            res.send("truned off");
+            break;
+         default:
+            break;
+      }
    });
    app.listen(3003);
 }
